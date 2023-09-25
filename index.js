14172a9e63eeaf363b0b6fe862f2df3ceb44755c
kriti bagga <bagga.kriti@gmail.com>
2023-09-25 17:36:17 -0400

    to do list

--- index.js
+++ index.js
@@ -3,12 +3,50 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const items = [];
+const workitems = [];
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-	res.render("index.ejs");
+	const today = new Date();
+	const options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+	};
+	const day = today.toLocaleDateString("en-US", options);
+
+	res.render("partials/list.ejs", {
+		listtitle: day,
+		newListItems: items,
+	});
+});
+
+app.post("/", (req, res) => {
+	const item = req.body.newItem;
+	if (req.body.list === "Work") {
+		workitems.push(item);
+		res.redirect("/work");
+	} else {
+		items.push(item);
+		res.redirect("/");
+	}
+});
+
+app.get("/work", (req, res) => {
+	res.render("partials/list.ejs", {
+		listtitle: "Work List",
+		newListItems: workitems,
+	});
+});
+
+app.post("/work", (req, res) => {
+	const item = req.body.newItem;
+	workitems.push(item);
+	res.redirect("/work");
 });
 
 app.listen(port, () => {
